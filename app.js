951c3b97bb2e455bde10852e18311761f07e364c
eaj4 <70153018+eaj4@users.noreply.github.com>
2021-11-30 10:19:34 -0500

    Adding first API

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const indexRouter = require('./routes/index');
+const randomRouter = require("./routes/random");
+
+app.use('/random/:type', randomRouter);
+app.use('/', indexRouter);
+
+app.use(function (req, res, next) {
+	res.sendStatus(404);
+});
+
+app.use(function (err, req, res, next) {
+	if (err) res.sendStatus(500);
+});
+
+module.exports = app;
